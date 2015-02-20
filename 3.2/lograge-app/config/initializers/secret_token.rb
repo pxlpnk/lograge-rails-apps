3dd670d99b45ac8c2568cafc9a578ef27270a907
Andreas Tiefenthaler <at@an-ti.eu>
2015-02-20 22:53:00 +0100

    initial

---
+++ 3.2/lograge-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogrageApp::Application.config.secret_token = '3ea2524ac6c804808e176d2eb5c7dce43bdf38476e92a64c2462270baa01b15def0b8ea373b3f032ccd63e5426f8744bbc67157c5468dec9e4f2e04b25086e8c'
